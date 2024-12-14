8b45aa1a82f1eb49c2d6fb869ff0fafba49c6242
m-housni <mohamedhousni1000k@gmail.com>
2024-12-14 01:46:43 +0100

    feat(FE Partie 2): Le formulaire doit permettre de saisir son email, un message et de cliquer sur "Envoyer" + mail et message doivent être obligatoirement remplis, message doit être inférieur à 300 caractères.

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./shared/features/home/home.component";
 import { ContactComponent } from "./shared/features/contact/contact.component";
+import { CartComponent } from "./shared/features/cart/cart.component";
 
 export const APP_ROUTES: Routes = [
   {
@@ -11,6 +12,10 @@ export const APP_ROUTES: Routes = [
     path: "products",
     loadChildren: () =>
       import("./products/products.routes").then((m) => m.PRODUCTS_ROUTES)
+  },
+  {
+    path: "cart",
+    component: CartComponent,
   },
   {
     path: "contact",
