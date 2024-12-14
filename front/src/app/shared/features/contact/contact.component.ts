e9cd2444cddb158006a9d94bc419c26c53fa054f
m-housni <mohamedhousni1000k@gmail.com>
2024-12-14 10:03:07 +0100

    feat(FE Partie 2): Quand le message a été envoyé, afficher un message à l'utilisateur : Demande de contact envoyée avec succès.

--- front/src/app/shared/features/contact/contact.component.ts
+++ front/src/app/shared/features/contact/contact.component.ts
@@ -1,40 +1,55 @@
-import { Component, inject, Output } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Component } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
-import { CartService } from "../../../cart/data-access/cart.service"; // Correct import path for CartService
-import { Product } from "app/products/data-access/product.model";
 
 @Component({
   selector: "app-contact",
-  templateUrl: "./contact.component.html",
+  template: `<p-card header="Contact" class="block m-2 text-left">
+  <div class="m-0">
+    <!-- form -->
+    <form #form="ngForm" (ngSubmit)="onSubmit(form)" class="p-fluid p-formgrid p-grid {{ isVisible ? '' : 'hidden' }}">
+      <div class="p-field p-col-12">
+        <label for="email" class="p-col-12 p-md-2">Email</label>
+        <div class="p-col-12 p-md-10">
+          <input id="email" [placeholder]="formData.email" name="email" ngModel type="email" pInputText class="p-inputtext p-component" required email />
+        </div>
+      </div>
+      <div class="p-field p-col-12">
+        <label for="message" class="p-col-12 p-md-2">Message</label>
+        <div class="p-col-12 p-md-10">
+          <textarea id="message" [placeholder]="formData.text" name="message" ngModel rows="5" pInputTextarea class="p-inputtextarea p-component" required maxlength="300"></textarea>
+        </div>
+      </div>
+      <div class="p-field p-col-12 p-md-10 p-offset-md-2">
+        <button type="submit" pButton label="Envoyer" class="p-button p-component" [disabled]="form.invalid"></button>
+      </div>
+    </form>
+    <div class="p-fluid p-formgrid p-grid {{ isSuccess ? '' : 'hidden' }}">Demande de contact envoyée avec succès</div>
+  </div>
+</p-card>`,
   styleUrls: ["./contact.component.scss"],
   standalone: true,
-  imports: [CardModule, RouterLink, ButtonModule],
+  imports: [CardModule, FormsModule, ButtonModule],
 })
 export class ContactComponent {
 
+  isVisible: boolean = true;
+  isSuccess: boolean = false;
 
-  private readonly cartService = inject(CartService);
-  public products: Product[] = [];
-  public quantities: number[] = [];
-  public cart: any[] = [];
-
-  constructor() {
-    this.cartService.getCart().subscribe(cart => {
-      let products = Array.from(cart.keys());
-      let quantities = Array.from(cart.values());
-      this.products = products;
-      this.quantities = quantities;
-      this.cart = products.map((p, index) => [p, quantities[index]])
-    });
+  public formData = {
+    email: "email@mail.com",
+    text: "lorem ipsum ...",
   }
 
-  public onDelete(product: Product) {
-    // delete product
-    this.cartService.removeProductFromCart(product).subscribe(cart => {
+  public onSubmit(form: NgForm): void {
+    if (!form.valid) return;
+    // hide form
+    this.isVisible = false;
+    // handle data
 
-    });
-    //alert("deleting product ..");
+    // show success message
+    this.isSuccess = true;
+
   }
 }
